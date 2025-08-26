aae2e0cd1b1608d97f883eb102e262b992340192
Benoît Legat <benoit.legat@gmail.com>
2025-08-26 07:31:21 +0200

    Fix format

--- src/reverse_mode.jl
+++ src/reverse_mode.jl
@@ -350,10 +350,11 @@ function _reverse_eval(f::_SubexpressionStorage)
         children_indices = SparseArrays.nzrange(f.adj, k)
         if node.type == MOI.Nonlinear.NODE_CALL_MULTIVARIATE
             if node.index in
-                eachindex(MOI.Nonlinear.DEFAULT_MULTIVARIATE_OPERATORS)
+               eachindex(MOI.Nonlinear.DEFAULT_MULTIVARIATE_OPERATORS)
                 op = MOI.Nonlinear.DEFAULT_MULTIVARIATE_OPERATORS[node.index]
                 if op == :vect
-                    @assert _eachindex(f.sizes, k) == eachindex(children_indices)
+                    @assert _eachindex(f.sizes, k) ==
+                            eachindex(children_indices)
                     for j in eachindex(children_indices)
                         rev_parent = @s f.reverse_storage[k]
                         ix = children_arr[children_indices[j]]
