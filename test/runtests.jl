42417de2ecbd3e9d5ab424f955fd33f86da0bd29
Benoît Legat <benoit.legat@gmail.com>
2026-05-05 09:20:54 +0200

    Fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
 include("ReverseAD.jl")
 include("ArrayDiff.jl")
 include("JuMP.jl")
-# Currently needs https://github.com/jump-dev/NLopt.jl/pull/273
-#include("NLopt.jl")
-include("NLPModelsJuMP.jl")
-include("Optimisers.jl")
+if VERSION > v"1.10"
+    # [sources] not supported on Julia v1.10
+    # Needs https://github.com/jump-dev/NLopt.jl/pull/273
+    include("NLopt.jl")
+    # Needs https://github.com/JuliaSmoothOptimizers/NLPModelsJuMP.jl/pull/229
+    include("NLPModelsJuMP.jl")
+    include("Optimisers.jl")
+end
