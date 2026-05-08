ae20ce9ecdde7e77a329ac5b20697891efef2b7a
Benoît Legat <benoit.legat@gmail.com>
2026-05-08 09:01:16 +0200

    Improve bench script

--- perf/neural_bench.jl
+++ perf/neural_bench.jl
@@ -3,23 +3,41 @@ include("hand_cuda.jl")
 include("pytorch.jl")
 include("arraydiff.jl")
 
+function _display(name, bench)
+    println("### $name")
+    println("```")
+    display(bench)
+    println("```")
+    return
+end
+
 function compare(T, h, d, n; gpu)
-    println("Lux")
-    display(LuxNeural.neural(T, h, d, n; gpu))
-    println("Hand-CUDA without prealloc")
-    display(HandCuda.neural(T, h, d, n; prealloc = false, gpu))
-    println("Hand-CUDA with prealloc")
-    display(HandCuda.neural(T, h, d, n; prealloc = true, gpu))
-    println("PyTorch eager")
-    display(PyTorchNeural.neural(T, h, d, n; eager = true, gpu))
-    println("PyTorch compiled")
-    display(PyTorchNeural.neural(T, h, d, n; eager = false, gpu))
-    println("ArrayDiff")
-    return display(ArrayDiffNeural.neural(T, h, d, n; gpu))
+    _display("Lux", LuxNeural.neural(T, h, d, n; gpu))
+    _display(
+        "Hand-CUDA without prealloc",
+        HandCuda.neural(T, h, d, n; prealloc = false, gpu),
+    )
+    _display(
+        "Hand-CUDA with prealloc",
+        HandCuda.neural(T, h, d, n; prealloc = true, gpu),
+    )
+    _display(
+        "PyTorch eager",
+        PyTorchNeural.neural(T, h, d, n; eager = true, gpu),
+    )
+    _display(
+        "PyTorch compiled",
+        PyTorchNeural.neural(T, h, d, n; eager = false, gpu),
+    )
+    _display(
+        "ArrayDiff",
+        ArrayDiffNeural.neural(T, h, d, n; gpu),
+    )
+    return
 end
 
 T, h, d, n = Float32, 4096, 13, 178
-println("CPU")
+println("## CPU")
 compare(T, h, d, n; gpu = false)
-println("GPU")
+println("## GPU")
 compare(T, h, d, n; gpu = true)
