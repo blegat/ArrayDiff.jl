bdc0164a4e79dda585a4aee7cc64777c6513322d
Benoît Legat <benoit.legat@gmail.com>
2026-05-20 15:21:18 +0200

    fix

--- src/reverse_mode.jl
+++ src/reverse_mode.jl
@@ -540,7 +540,6 @@ function _reverse_broadcasted_mul(dout, dlhs, drhs, lhs, rhs)
     # `reverse_storage` is zeroed out at construction but if
     # at the second call of `eval_objective_gradient`, it is not
     # zero so we can't assume that it is zero
-    end
     fill!(dlhs, zero(eltype(dlhs)))
     # Would need `conj` once we support `Complex`
     Base.mapreducedim!(
