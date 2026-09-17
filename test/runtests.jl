b8e1764d4fe04ec61c9e0e265f2c5805aa9181c8
Benoît Legat <benoit.legat@gmail.com>
2026-09-17 14:13:38 +0200

    Fix format

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ is_test_file(f) = startswith(f, "test_") && endswith(f, ".jl")
 
 testsuite = Dict{String,Expr}()
 for file in filter(is_test_file, readdir(@__DIR__))
-    name = file[1:end-3]
+    name = file[1:(end-3)]
     if VERSION < v"1.11" && name in (
         # [sources] not supported on Julia v1.10
         # Needs https://github.com/jump-dev/NLopt.jl/pull/273
