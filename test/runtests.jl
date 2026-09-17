759b7dea565f1a31c4f23263e7dc072801e88588
Benoît Legat <benoit.legat@gmail.com>
2026-09-17 12:58:14 +0200

    Use ParallelTestRunner.jl to parallelize the tests
    Follows MathOptInterface (jump-dev/MathOptInterface#2932): test files are
    renamed to `test_*.jl` and discovered by `test/runtests.jl`, which hands
    them to `ParallelTestRunner.runtests`. Helper files (`Transformer.jl`,
    `OptimisersSolver.jl`) keep their names so they are not picked up as
    testsets, and the Julia v1.10 exclusions are kept.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,22 @@
-include("ReverseAD.jl")
-include("ArrayDiff.jl")
-include("JuMP.jl")
-include("MathOptAI.jl")
-include("ONNXExt.jl")
-if VERSION >= v"1.11"
-    # [sources] not supported on Julia v1.10
-    # Needs https://github.com/jump-dev/NLopt.jl/pull/273
-    include("NLopt.jl")
-    # Needs https://github.com/JuliaSmoothOptimizers/NLPModelsJuMP.jl/pull/229
-    include("NLPModelsJuMP.jl")
-    include("Optimisers.jl")
-    #include("Optimisers_GPU.jl")
+import ArrayDiff
+import ParallelTestRunner
+
+is_test_file(f) = startswith(f, "test_") && endswith(f, ".jl")
+
+testsuite = Dict{String,Expr}()
+for file in filter(is_test_file, readdir(@__DIR__))
+    name = file[1:end-3]
+    if VERSION < v"1.11" && name in (
+        # [sources] not supported on Julia v1.10
+        # Needs https://github.com/jump-dev/NLopt.jl/pull/273
+        "test_NLopt",
+        # Needs https://github.com/JuliaSmoothOptimizers/NLPModelsJuMP.jl/pull/229
+        "test_NLPModelsJuMP",
+        "test_Optimisers",
+    )
+        continue
+    end
+    testsuite[name] = :(include($(joinpath(@__DIR__, file))))
 end
-#include("eval_residual_gpu.jl")
+
+ParallelTestRunner.runtests(ArrayDiff, ARGS; testsuite)
